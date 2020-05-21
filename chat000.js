f0e81ecd5b5bc5e7e92763f8c231e413d0cdd737
ahwvk <65707335+ahwvk@users.noreply.github.com>
2020-05-21 18:16:44 +0300

    Create chat000.js

---
+++ chat000.js
@@ -0,0 +1,21 @@
+var _0x3311 = [".nav-tabs", "insertBefore", '<form name="jump">\r\n<div class="MyColors">\r\n\t\t\t\t\t<map id="ColorsMap" name="ColorsMap">\r\n\t\t\t\t\t<area alt="" coords="275, 0, 298, 25" href="javascript:chooseStyle(\'none\',60)" shape="rect" title="1">\r\n\t\t\t\t\t<area alt="" coords="244, 0, 268, 25" href="javascript:chooseStyle(\'Co2\',60)" shape="rect" title="2">\r\n\t\t\t\t\t<area alt="" coords="213, 0, 238, 25" href="javascript:chooseStyle(\'Co3\',60)" shape="rect" title="3">\r\n\t\t\t\t\t<area alt="" coords="183, 0, 207, 25" href="javascript:chooseStyle(\'Co4\',60)" shape="rect" title="4">\r\n\t\t\t\t\t<area alt="" coords="152, 0, 177, 25" href="javascript:chooseStyle(\'Co5\',60)" shape="rect" title="5">\r\n\t\t\t\t\t</map>\r\n\t\t\t <img alt="" height="25" src="https://mskchat.com/sico/z1e8ra970c10.gif" width="300" usemap="#ColorsMap"></div>\r\n</form>\r\n\r\n\r\n \r\n\r\n',
+"https://cdn.statically.io/gh/ahwvk/mod/a053e997/msk1.js", "getScript", ".loginstat", "insertAfter", "border-radius", "3px", "css", ".u-pic", "1px 0", "2px", "5px", ".hand.nosel.fl.uzr.border",
+"0px 0", ".uzr.fl.corner.borderg", '<input id="stealth" type="checkbox" value="" maxlength="12"><i class="fa fa-eye-slash" aria-hidden="true">', "html", ".checkbox label", "width", "100%", "fl", "addClass", "body", '<link rel="stylesheet" type="text/css" href="">\r\n\r\n\r\n<link href="https://cdn.statically.io/gh/ahwvk/msk/a0e5d3fb/7ob1.css" rel="stylesheet" type="text/css" />\r\n<link href="https://cdn.statically.io/gh/ahwvk/msk/a0e5d3fb/7ob2.css" media="screen" rel="alternate stylesheet" title="Co2" type="text/css" />\r\n<link href="https://cdn.statically.io/gh/ahwvk/msk/a0e5d3fb/7ob3.css" media="screen" rel="alternate stylesheet" title="Co3" type="text/css" />\r\n<link href="https://cdn.statically.io/gh/ahwvk/msk/a0e5d3fb/7ob4.css" media="screen" rel="alternate stylesheet" title="Co4" type="text/css" />\r\n<link href="https://github.com/ahwvk/msk/blob/master/7ob5.css" media="screen" rel="alternate stylesheet" title="Co5" type="text/css" />\r\n\r\n<style>\r\n\r\n.MyColors {\r\n position: absolute;\r\n transform: matrix(1, 0, 0, 1, 72, -2);\r\n margin-top: 2px;\r\n width: 300px;\r\n height: 60px;\r\n float: right;\r\n background: transparent url(https://mskchat.com/sico/z1e8oqk6pf10.png) no-repeat scroll right top;\r\n text-align: left;\r\n}\r\n\r\n\r\n\r\n#des0 {\r\n background-color: #3c3c3c;\r\n\tborder-radius: 15px 15px 0px 0!important;\r\n}\r\n\r\na.fa.fa-user-plus {\r\n color: #ff0000;\r\n}\r\n\r\n\r\nlabel.fl.nosel.label.pnhead {\r\nbackground-image: -webkit-linear-gradient(45deg,hsla(0,0%,100%,.2) 25%,transparent 0,transparent 50%,hsla(0,0%,100%,.2) 0,hsla(0,0%,100%,.2) 75%,transparent 0,transparent)!important;\r\n\r\n}\r\n\r\n\r\n\r\n\r\n#des3 {\r\n margin: 1px!important;\r\n}\r\n\r\nul.nav.nav-tabs.fl {\r\n margin-top: 1px!important;\r\n}\r\n\r\n\r\n\r\n\r\n</style>\r\n'];
+$("" + _0x3311[2])[_0x3311[1]](_0x3311[0]);
+$[_0x3311[4]](_0x3311[3]);
+$(_0x3311[7])[_0x3311[6]]($(_0x3311[5]));
+$(_0x3311[11])[_0x3311[10]](_0x3311[8], _0x3311[9]);
+$(_0x3311[15])[_0x3311[10]]({
+"margin" : _0x3311[12],
+paddingTop : _0x3311[13],
+borderRadius : _0x3311[14]
+});
+$(_0x3311[17])[_0x3311[10]]({
+"margin" : _0x3311[16],
+paddingTop : _0x3311[13],
+borderRadius : _0x3311[14]
+});
+$(_0x3311[20])[_0x3311[19]]("" + _0x3311[18]);
+$(_0x3311[0])[_0x3311[24]](_0x3311[23])[_0x3311[10]](_0x3311[21], _0x3311[22]);
+$(_0x3311[0])[_0x3311[24]](_0x3311[23])[_0x3311[10]](_0x3311[21], _0x3311[22]);
+$("" + _0x3311[26])[_0x3311[1]](_0x3311[25]);
