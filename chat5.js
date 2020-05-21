04ad347b4aaec8bd96f6523b445b56de7b351f3b
ahwvk <65707335+ahwvk@users.noreply.github.com>
2020-05-21 16:55:44 +0300

    Create chat5.js

---
+++ chat5.js
@@ -0,0 +1,20 @@
+var _0x27ec = [".nav-tabs", "insertBefore", '<form name="jump">\r\n<div class="des">\r\n\t\t\t\t\t<map id="ColorsMap" name="ColorsMap">\r\n\t\t\t\t\t<area alt="" coords="275, 0, 298, 25" href="javascript:chooseStyle(\'none\',60)" shape="rect" title="1">\r\n\t\t\t\t\t<area alt="" coords="244, 0, 268, 25" href="javascript:chooseStyle(\'Co2\',60)" shape="rect" title="2">\r\n\t\t\t\t\t<area alt="" coords="213, 0, 238, 25" href="javascript:chooseStyle(\'Co3\',60)" shape="rect" title="3">\r\n\t\t\t\t\t<area alt="" coords="183, 0, 207, 25" href="javascript:chooseStyle(\'Co4\',60)" shape="rect" title="4">\r\n\t\t\t\t\t<area alt="" coords="152, 0, 177, 25" href="javascript:chooseStyle(\'Co5\',60)" shape="rect" title="5">\r\n\t\t\t\t\t</map>\r\n\t\t\t <img alt="" height="25" src="https://mskchat.com/sico/z1e8ra970c10.gif" width="300" usemap="#ColorsMap"></div>\r\n</form>\r\n\r\n',
+"https://cdn.statically.io/gh/ahwvk/msk/f7ef8694/chat1.js", "getScript", "width", "100%", "css", "fl", "addClass", "body", '<link rel="stylesheet" type="text/css" href="">\r\n<link rel="stylesheet" href="">\r\n<link href="https://cdn.statically.io/gh/ahwvk/msk/f7ef8694/7ob1.css" rel="stylesheet" type="text/css" />\r\n<link href="https://cdn.statically.io/gh/ahwvk/msk/f7ef8694/7ob2.css" media="screen" rel="alternate stylesheet" title="Co2" type="text/css" />\r\n<link href="https://cdn.statically.io/gh/ahwvk/msk/f7ef8694/7ob3.css" media="screen" rel="alternate stylesheet" title="Co3" type="text/css" />\r\n<link href="https://cdn.statically.io/gh/ahwvk/msk/f7ef8694/7ob4.css" media="screen" rel="alternate stylesheet" title="Co4" type="text/css" />\r\n<link href="https://cdn.statically.io/gh/ahwvk/msk/f7ef8694/7ob5.css" media="screen" rel="alternate stylesheet" title="Co5" type="text/css" />\r\n<style>\r\n\r\n</style>\r\n'];
+$("" + _0x3311[2])[_0x3311[1]](_0x3311[0]);
+$[_0x3311[4]](_0x3311[3]);
+$(_0x3311[7])[_0x3311[6]]($(_0x3311[5]));
+$(_0x3311[11])[_0x3311[10]](_0x3311[8], _0x3311[9]);
+$(_0x3311[15])[_0x3311[10]]({
+"margin" : _0x3311[12],
+paddingTop : _0x3311[13],
+borderRadius : _0x3311[14]
+});
+$(_0x3311[17])[_0x3311[10]]({
+"margin" : _0x3311[16],
+paddingTop : _0x3311[13],
+borderRadius : _0x3311[14]
+});
+$(_0x3311[20])[_0x3311[19]]("" + _0x3311[18]);
+$(_0x3311[0])[_0x3311[24]](_0x3311[23])[_0x3311[10]](_0x3311[21], _0x3311[22]);
+$(_0x3311[0])[_0x3311[24]](_0x3311[23])[_0x3311[10]](_0x3311[21], _0x3311[22]);
+$("" + _0x3311[26])[_0x3311[1]](_0x3311[25]);
